9fc0b2d5d6b1a5dece25cdda4b8807e11241d2f0
jaeun <wodns617@gmail.com>
2021-08-31 22:13:46 +0900

    first commit

---
+++ 재귀/factorial/Main.java
@@ -0,0 +1,29 @@
+package 재귀.factorial;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int ans;
+        int n=-1;
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        ans = new Factorial().run(n);
+        System.out.println(ans);
+    }
+}
+
+class Factorial{
+    int sum = 1;
+    public int run(int n){
+        if(n==1 ||n==0){
+            return sum;
+        }
+        sum*=n;
+        return run(n-1);
+    }
+}
+
+

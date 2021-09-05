77a545d6e697a2bbeab4afc7fa69ab0ba1aa5652
Jaeun-Choi98 <chlwodns98@naver.com>
2021-09-05 11:09:57 +0900

    five commit

--- src/bruteforce/블랙잭/Main.java
+++ src/bruteforce/블랙잭/Main.java
@@ -10,35 +10,44 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        cardNum = sc.nextInt();
+        cardNum = sc.nextInt(); M = sc.nextInt();
         cardList = new int[cardNum];
 
         for(int i=0;i<cardNum;i++) cardList[i] = sc.nextInt();
 
         BlackJac blackJac = new BlackJac();
-        blackJac.run(cardList);
+        blackJac.run(cardList, M);
         System.out.println(blackJac.toString());
     }
 }
 
 class BlackJac{
-    int[] answer;
+    Integer maxSum;
 
     public BlackJac(){
-        answer = new int[3];
-        for(int i=0;i<3;i++) answer[i] = 1;
+        maxSum = 9;
     }
 
-    public void run(int[] cardList){
+    public void run(int[] cardList, int M){
+        for(int i=0;i<cardList.length;i++){
+            for(int j=i+1;j<cardList.length;j++){
+                for(int k =j+1;k<cardList.length;k++){
+                    if(sum(cardList,i,j,k)<=M){
+                        if(maxSum < sum(cardList,i,j,k)) maxSum = sum(cardList, i, j, k);
+                    }
+                }
+            }
+        }
+    }
 
+    private int sum(int[] charArr, int a, int b, int c){
+        int sum =0;
+        sum = charArr[a] + charArr[b] + charArr[c];
+        return sum;
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<3;i++){
-            sb.append(answer[i]); sb.append('\n');
-        }
-        return sb.toString();
+        return maxSum.toString();
     }
 }

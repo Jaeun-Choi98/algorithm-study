800eb8906d7ab2c54997e9ce0a5de91fd80bf709
Jaeun-Choi98 <wodns617@gmail.com>
2021-11-02 22:54:29 +0900

    34 commit

---
+++ src/시물레이션/스티커붙이기/Main.java
@@ -0,0 +1,13 @@
+package 시물레이션.스티커붙이기;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[][] arr,copyArr;
+    static int N,M,stickerNum;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}

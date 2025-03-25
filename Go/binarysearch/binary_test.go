d5afb82a1ce8905acf5ef08a2a44603d271e90af
Jaeun-Choi98 <wodns617@gmail.com>
2025-03-25 14:08:16 +0900

    modify test code

--- Go/binarysearch/binary_test.go
+++ Go/binarysearch/binary_test.go
@@ -10,10 +10,10 @@ import (
 func testFunc() []int {
 	// 정렬되지 않은 데이터 -> 정렬 후: {1,4,4,4,5,6}
 	data := []int{1, 6, 4, 5, 4, 4}
-	return []int{LowerSearch(data, 4), UpperSearch(data, 4)}
+	return []int{data[LowerSearch(data, 4)], data[UpperSearch(data, 4)]}
 }
 
 func TestTestFunc(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal([]int{1, 3}, testFunc())
+	assert.Equal([]int{4, 5}, testFunc())
 }
